a60884f0d3d2672ff9709130dfe6ae290c59b1c9
OTB User <office@otb.expert>
2019-04-23 01:34:47 +0100

    New features added: Multiple item sources, Tags Code is still spaghetti

---
+++ Ajuro.Notes/Model/DirectoryItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoDrops.Model
+{
+	public class DirectoryItem : FileItem
+	{
+		public List<FileItem> Items { get; set; }
+
+		public DirectoryItem()
+		{
+			Items = new List<FileItem>();
+		}
+	}
+}
